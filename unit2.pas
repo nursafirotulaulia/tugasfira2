1316e39a832a6e901066a7ca1c64125aa341cb45
nursafirotulaulia <kyufira@yahoo.com>
2015-06-11 21:36:20 +0700

    tugasfira2
    tugasfira2

---
+++ unit2.pas
@@ -0,0 +1,40 @@
+unit unit2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    fira: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Label1.caption:=(fira.text);
+end;
+
+end.
+
